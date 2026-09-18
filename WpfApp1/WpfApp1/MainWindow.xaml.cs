08e7014af294888e5b7aa1ce651e7b6c5d63ad60
Dominik015 <158041727+Dominik015@users.noreply.github.com>
2026-09-18 09:51:31 +0200

    Add files via upload

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOsszead_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                txtEredmeny.Content = Convert.ToDouble(txtA.Text) + Convert.ToDouble(txtB.Text);
+            }
+            catch (FormatException FormEx)
+            {
+                MessageBox.Show(FormEx.Message);
+
+            }
+        }
+
+        private void btnKivon_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                txtEredmeny.Content = Convert.ToDouble(txtA.Text) - Convert.ToDouble(txtB.Text);
+            }
+            catch (FormatException FormEx)
+            {
+                MessageBox.Show(FormEx.Message);
+            }
+        }
+
+        private void btnSzoroz_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                txtEredmeny.Content = Convert.ToDouble(txtA.Text) * Convert.ToDouble(txtB.Text);
+            }
+            catch (FormatException FormEx)
+            {
+                MessageBox.Show(FormEx.Message);
+            }
+        }
+
+        private void btnOsztas_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                txtEredmeny.Content = Convert.ToDouble(txtA.Text) / Convert.ToDouble(txtB.Text);
+            }
+            catch (FormatException FormEx)
+            {
+                MessageBox.Show(FormEx.Message);
+            }
+        }
+    }
+}
